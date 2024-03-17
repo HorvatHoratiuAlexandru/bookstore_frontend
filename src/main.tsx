86d42c093894849161bd52691f48d91e53c13cd1
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-03-17 09:25:54 +0200

    store and router set up

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,13 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
