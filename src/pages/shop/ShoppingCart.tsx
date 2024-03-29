35ba9b4bd0c863c6286ba35083d87fe1ab38e27e
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-03-29 13:25:43 +0200

    ordering steps layout added

--- src/pages/shop/ShoppingCart.tsx
+++ src/pages/shop/ShoppingCart.tsx
@@ -1,15 +1,23 @@
 import {
   Avatar,
   Box,
+  Button,
+  Checkbox,
   Container,
   Divider,
+  FormControlLabel,
   IconButton,
+  Input,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
   Paper,
   Stack,
+  Step,
+  StepLabel,
+  Stepper,
+  TextField,
   Typography,
 } from "@mui/material";
 
@@ -25,6 +33,10 @@ import {
   removeAllItems,
   removeItem,
 } from "../../store/shoppingcart/shoppingcartSlice";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const steps = ["Billing Data", "Card Payment", "Confirm"];
 
 const ShoppingCart = () => {
   const cartData = useSelector((rootState: RootState) => rootState.cartData);
@@ -40,6 +52,65 @@ const ShoppingCart = () => {
 
   const handleDelete = (key: string) => {
     dispatch(removeAllItems(key));
+  };
+
+  //STEPPER
+  // type OrderDetails = {
+  //   email: string;
+  //   fullName: string;
+  //   address: string;
+  //   cardNumber?: string;
+  //   ccv?: string;
+  //   cardHolderName?: string;
+  // };
+  // const { orderDetail, setOrderDetail } = useState<OrderDetails>({
+  //   email: "Mock",
+  //   fullName: "MockFullName",
+  //   address: "MockAddress",
+  // });
+
+  // const bilingDataExists;
+  // const cardDataExists;
+
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set<number>());
+
+  const isStepOptional = (step: number) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
   };
 
   return (
@@ -134,11 +205,178 @@ const ShoppingCart = () => {
             )}
           </List>
         </Paper>
-        <Box>
+        {Object.keys(cartData).length !== 0 && (
           <Paper>
-            <Typography>NextStep</Typography>
+            <Container maxWidth={"xs"}>
+              <Box
+                padding={3}
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                flexDirection={"column"}
+                columnGap={1}
+              >
+                <Typography gutterBottom variant="h6" color={"primary"}>
+                  Place Order:
+                </Typography>
+                <Box sx={{ width: "100%" }}>
+                  <Stepper activeStep={activeStep}>
+                    {steps.map((label, index) => {
+                      const stepProps: { completed?: boolean } = {};
+                      const labelProps: {
+                        optional?: React.ReactNode;
+                      } = {};
+                      if (isStepOptional(index)) {
+                        labelProps.optional = (
+                          <Typography variant="caption">Optional</Typography>
+                        );
+                      }
+                      if (isStepSkipped(index)) {
+                        stepProps.completed = false;
+                      }
+                      return (
+                        <Step key={label} {...stepProps}>
+                          <StepLabel {...labelProps}>{label}</StepLabel>
+                        </Step>
+                      );
+                    })}
+                  </Stepper>
+                  {activeStep === steps.length ? (
+                    <>
+                      <Typography
+                        variant="h6"
+                        color={"green"}
+                        sx={{ mt: 2, mb: 1 }}
+                      >
+                        Order Placed
+                      </Typography>
+                      <Box
+                        sx={{ display: "flex", flexDirection: "row", pt: 2 }}
+                      >
+                        <Box sx={{ flex: "1 1 auto" }} />
+                        <Link to="/">
+                          <Button>Go back to main page</Button>
+                        </Link>
+                      </Box>
+                    </>
+                  ) : (
+                    <>
+                      {activeStep === 0 && (
+                        <Box sx={{ width: "100%" }}>
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                          />
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="address"
+                            label="Address"
+                            name="address"
+                            autoComplete="address"
+                            autoFocus
+                          />
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Full Name"
+                            name="name"
+                            autoComplete="FullName"
+                            autoFocus
+                          />
+                        </Box>
+                      )}
+                      {activeStep === 1 && (
+                        <Box sx={{ width: "100%" }}>
+                          <Typography variant="body2" color={"text.secondary"}>
+                            Mock data with whatever you like
+                          </Typography>
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="cnumber"
+                            label="Card Number"
+                            name="cnumber"
+                            autoComplete="Card Number"
+                            autoFocus
+                          />
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="cname"
+                            label="Card Holder Full Name"
+                            name="cname"
+                            autoComplete="FullName"
+                            autoFocus
+                          />
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="ccv"
+                            label="CCV"
+                            name="ccv"
+                            autoComplete="ccv"
+                            autoFocus
+                          />
+                          <Typography variant="body2" color={"text.secondary"}>
+                            Press skip if you want to pay on delivery*
+                          </Typography>
+                        </Box>
+                      )}
+                      {activeStep === 2 && (
+                        <Box marginY={10} sx={{ width: "100%" }}>
+                          <Typography variant="h6" color={"primary"}>
+                            Place order by pressing finish
+                          </Typography>
+                        </Box>
+                      )}
+                      <Typography sx={{ mt: 2, mb: 1 }}>
+                        Step {activeStep + 1}
+                      </Typography>
+                      <Box
+                        sx={{ display: "flex", flexDirection: "row", pt: 2 }}
+                      >
+                        <Button
+                          color="inherit"
+                          disabled={activeStep === 0}
+                          onClick={handleBack}
+                          sx={{ mr: 1 }}
+                        >
+                          Back
+                        </Button>
+                        <Box sx={{ flex: "1 1 auto" }} />
+                        {isStepOptional(activeStep) && (
+                          <Button
+                            color="inherit"
+                            onClick={handleSkip}
+                            sx={{ mr: 1 }}
+                          >
+                            Skip
+                          </Button>
+                        )}
+                        <Button onClick={handleNext}>
+                          {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                        </Button>
+                      </Box>
+                    </>
+                  )}
+                </Box>
+              </Box>
+            </Container>
           </Paper>
-        </Box>
+        )}
       </Stack>
     </Container>
   );
